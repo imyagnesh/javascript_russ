fc1ccd5ab6993cc5f062dc1c34a4e7a34e8fc6b9
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-12-08 20:04:38 +0530

    comit code

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const label = document.getElementById('label');
-if(label) {
-    label.style = 'color:red';
-}
+// import { add,greet, message, text } from './src/test'
+import * as test from './src/test'
+
+// import xyz, { text} from './src/test';
+
+console.log(test.greet)
+console.log(test.message);
+const res = test.add(1, 2);
+console.log(res);
+
+console.log(test.text)
+
+// const label = document.getElementById('label');
+// if(label) {
+//     label.style = 'color:red';
+// }
---
+++ src/test.js
@@ -0,0 +1,7 @@
+export const greet = "Hello";
+export const message = "This text is from test.js file";
+export const add = (a, b) => a + b;
+
+export const text = "exported text"
+
+export default { greet, message, add };
