5b1817e7a6b53589c99935eb220f57bc9e6983a0
imyagnesh <yagnesh.modh@gmail.com>
2020-12-15 21:19:16 +0530

    commit code

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,10 @@ module.exports = {
     filename: 'bundle.js',
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }],
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.css$/, exclude: /node_modules/, use: ['style-loader', 'css-loader'] },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
